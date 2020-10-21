f94459de31cf666f8232485eb702b6d02f6d2c69
Maurice <maurice.molin@gmail.com>
2020-10-21 01:10:00 -0400

    "First true commit of project base."

---
+++ README.md
@@ -0,0 +1,2 @@
+#Will like to hear a joke
+Then is the app for you. It displays a random Joke on the index.
---
+++ src/test/java/tost/springframework/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package tost.springframework.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
